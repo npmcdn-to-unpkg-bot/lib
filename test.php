af8e3d01a1ded5c02c62a8c6fd25c2e09ab1b061
karthikchandransmaat <karthik.c@smaatapps.com>
2016-06-15 12:53:46 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,13 @@
 <?php
-	echo "check";
+	$from_email = "admin@smaatapps.com";
+	$message = "Hi";
+	$subject = "Test";
+	
+	$to = "car3chan@gmail.com";
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: <'.$from_email.'>' . "\r\n";
+	$headers .= 'Cc: '.$from_email.'' . "\r\n";
+	$headers .= 'Reply-To: <'.$from_email.'>' . "\r\n"; 
+	mail($to,$subject,$message,$headers);
  ?>
