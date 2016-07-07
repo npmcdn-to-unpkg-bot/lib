c58c7cdceca4ee3b319da9eccb7f3a1b0be31c37
kumar31 <kumarappan@smaatapps.com>
2016-07-07 18:26:43 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,13 +1,3 @@
 <?php
-	$host = "dtdb-dev.chibwsimd7gb.ap-southeast-1.rds.amazonaws.com:3306";
-	$dbuser ="dtuser";
-	$db_password ="Ze5rewefEc";
-	$database="dogytales";
-	
-    	mysql_connect($host,$dbuser,$db_password) or die ("Error connecting to DB");
-	$selectDb=mysql_select_db($database) ;	
-	if($selectDb)
-	{
-		echo "successfully done";	
-	}
+echo "as";
 ?>
