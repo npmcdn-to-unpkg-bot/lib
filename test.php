458d6e60f53f3dbbcf4289bf72dbff3185855080
kumar31 <kumarappan@smaatapps.com>
2016-07-07 19:06:23 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,19 @@
 <?php
 	$link = mysqli_connect('mydbinstance.abcdefghijkl.us-east-1.rds.amazonaws.com', 'awsuser', 'mypassword', 'information_schema', 3306);
 	var_dump($link);
+	 $host = "mydbinstance.abcdefghijkl.us-east-1.rds.amazonaws.com";
+	   
+	   
+
+		$dbuser ="awsuser";
+	 	$db_password ="mypassword";
+		$database="information_schema";
+
+	 
+    mysql_connect($host,$dbuser,$db_password) or die ("Error connecting to DB");
+	$selectDb=mysql_select_db($database) ;	
+	if($selectDb)
+	{
+		echo "successfully done";	
+	}
 ?>
