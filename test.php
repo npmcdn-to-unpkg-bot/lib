b7fceb04e087db1afc48e498e86714bb9c8fbfd5
kumar31 <kumarappan@smaatapps.com>
2016-07-07 19:38:08 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,28 +1,8 @@
 <?php
 	$link = mysqli_connect('soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com', 'soiree', 'kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9', 'soiree_smaatapp',3306);
 	var_dump($link);
-	$result = $mysqli->query("SHOW TABLES");
-    while ( $row = $result->fetch_row() ){
-    $table = $row[0];
- echo '<h3>',$table,'</h3>';
-$result1 = $mysqli->query("SELECT * FROM $table");
-if($result1) {
-    echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-    $column = $mysqli->query("SHOW COLUMNS FROM $table");
-echo '<tr>';
-    while($row3 = $column->fetch_row() ) {
-    echo '<th>'.$row3[0].'</th>';
-}
-echo '</tr>';
-    while($row2 = $result1->fetch_row() ) {
-      echo '<tr>';
-      foreach($row2 as $key=>$value) {
-        echo '<td>',$value,'</td>';
-      }
-      echo '</tr>';
-    }
-    echo '</table><br />';
-  }
-  }
-$mysqli->close();
+	if (mysqli_connect_errno())
+	  {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }
 ?>
