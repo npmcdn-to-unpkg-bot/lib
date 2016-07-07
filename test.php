e95ecd61b11039440095b9e30b2a5a7de772a4cb
kumar31 <kumarappan@smaatapps.com>
2016-07-07 19:29:53 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,5 @@
 <?php
-	$link = mysqli_connect('mydbinstance.abcdefghijkl.us-east-1.rds.amazonaws.com', 'awsuser', 'mypassword', 'information_schema');
+	$link = mysqli_connect('soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com', 'soiree', 'kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9', 'soiree_smaatapp',3306);
 	var_dump($link);
-	 $host = "mydbinstance.abcdefghijkl.us-east-1.rds.amazonaws.com";
-	   
-	   
-
-		$dbuser ="awsuser";
-	 	$db_password ="mypassword";
-		$database="information_schema";
-
-	 
-    mysql_connect($host,$dbuser,$db_password) or die ("Error connecting to DB");
-	$selectDb=mysql_select_db($database) ;	
-	if($selectDb)
-	{
-		echo "successfully done";	
-	}else{
-		echo mysql_error();
-	}
+	
 ?>
