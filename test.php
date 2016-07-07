3c7ba2517ee074c98f479df2d633079b36ad8959
kumar31 <kumarappan@smaatapps.com>
2016-07-07 19:39:32 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
 <?php
 	$link = mysqli_connect('soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com', 'soiree', 'kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9', 'soiree_smaatapp',3306);
-	var_dump($link);
+	
 	if (mysqli_connect_errno())
 	  {
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	  }else{
+	  	echo "success";
 	  }
 ?>
