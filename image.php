93a6f4622aa030a1820852f359504745ce3bee69
kumar31 <kumarappan@smaatapps.com>
2016-07-08 15:33:42 +0530

    Update image.php

--- image.php
+++ image.php
@@ -9,5 +9,8 @@
 <script src="http://demo.phpgang.com/upload-multiple-images-jquery-ajax-using-php/js/jquery-1.8.0.min.js"></script>
 
 <script>
-alert('as');
+$('#upload').on('change', function() { 
+ alert('as');
+    
+});
 </script>
