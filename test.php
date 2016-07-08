37c1d3d0b2a51a3cd5f0492ca7812fe968044727
kumar31 <kumarappan@smaatapps.com>
2016-07-08 16:14:44 +0530

    Update test.php

--- test.php
+++ test.php
@@ -11,9 +11,10 @@
 	$username = "soiree";
 	$password = "kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9";
 	$dbname = "soiree_smaatapp";
+	$dbport = "3306";
 	
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli($servername, $username, $password, $dbname, $dbport);
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
