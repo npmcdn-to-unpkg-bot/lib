203f5deb897f2d95c602caed24f56e4beb237ff1
kumar31 <kumarappan@smaatapps.com>
2016-07-08 16:31:03 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php
-	/*$link = mysqli_connect('soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com', 'soiree', 'kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9', 'soiree_smaatapp',3306);
+	$link = mysqli_connect('soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com', 'soiree', 'kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9', 'soiree_smaatapp',3306);
 	
 	if (mysqli_connect_errno())
 	  {
 	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	  }else{
 	  	echo "success";
-	  }*/
+	  }
 	  
 	$servername = "soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com";
 	$username = "soiree";
