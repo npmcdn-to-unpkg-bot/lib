37aecbf2aeeeac27ee208869f077e23e5f32101d
kumar31 <kumarappan@smaatapps.com>
2016-07-08 16:28:45 +0530

    Update test.php

--- test.php
+++ test.php
@@ -7,31 +7,18 @@
 	  }else{
 	  	echo "success";
 	  }*/
+	  
 	$servername = "soiree-smaatapp.cvjwbf8cmrzh.us-west-2.rds.amazonaws.com";
 	$username = "soiree";
 	$password = "kQ#$U3GYUhmw=J&o6Rzv3uh4puWkZi[Nca3rPqBg9";
 	$dbname = "soiree_smaatapp";
 	$dbport = 3306;
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname, $dbport);
+	$con = mysqli_connect($servername, $username, $password, $dbname, $dbport);
+
 	// Check connection
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	} 
-	 $sql = "CREATE TABLE MyGuests (
-		id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-		firstname VARCHAR(30) NOT NULL,
-		lastname VARCHAR(30) NOT NULL,
-		email VARCHAR(50),
-		reg_date TIMESTAMP
-		)";
-		
-		if ($conn->query($sql) === TRUE) {
-		    echo "Table MyGuests created successfully";
-		} else {
-		    echo "Error creating table: " . $conn->error;
-		}
-		
-		$conn->close();
+	if (mysqli_connect_errno())
+	{
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
 ?>
