3a8bfe0f4f3db7750a5ecb3feac7c201f684a955
kumar31 <kumarappan@smaatapps.com>
2016-07-12 14:53:00 +0530

    Update test.php

--- test.php
+++ test.php
@@ -15,15 +15,18 @@
 	} else {
 	    echo "Error creating table: " . mysqli_error($link);
 	}*/
-	$query = "CREATE TABLE IF NOT EXISTS `client_card_details` (
-  `card_id` int(100) NOT NULL AUTO_INCREMENT,
+	$query = "CREATE TABLE IF NOT EXISTS `client_payment_details` (
+  `client_payment_details_id` int(11) NOT NULL AUTO_INCREMENT,
   `client_id` int(100) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `number` varchar(255) NOT NULL,
-  `cvv` varchar(255) NOT NULL,
-  `expiration_month` int(11) NOT NULL,
-  `expiration_year` int(11) NOT NULL,
-  PRIMARY KEY (`card_id`)
+  `event_id` int(100) NOT NULL,
+  `no_of_talents_requested` varchar(255) NOT NULL,
+  `no_of_talents_hired` int(100) NOT NULL,
+  `transaction_id` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `amount` float NOT NULL,
+  `datetime` datetime NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`checkinid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1";
 	if (mysqli_query($link, $query)) {
 	    echo "Table MyGuests created successfully";
